60770a627be5b285ebfc6cc71528eb97848776a1
gaurav <gks.com131@gmail.com>
2021-03-20 21:02:43 +0530

    simple jinja templating

--- __init__.py
+++ __init__.py
@@ -1,1 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/<name>")
+def root(name):
+    a = ["sai", "naresh", "sreekanth", "vijay", "vinay"] + [name]
+    return render_template("home.html", names=a)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
